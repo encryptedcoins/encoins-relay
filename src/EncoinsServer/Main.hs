df7ec462383cf94e5390ece7737f3feb71217cb8
encoins <111982001+encryptedcoins@users.noreply.github.com>
2023-02-12 18:33:51 +0100

    Clean up Main.hs

--- src/EncoinsServer/Main.hs
+++ src/EncoinsServer/Main.hs
@@ -5,42 +5,39 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE TupleSections         #-}
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
 module EncoinsServer.Main where
 
-import           Control.Exception                 (throw)
-import           Control.Monad                     (void)
-import           Control.Monad.Reader              (asks, liftIO)
-import           Data.Default                      (def)
-import           Data.Map                          (fromList)
-import           Data.Maybe                        (fromJust)
-import           Ledger                            (TxOutRef (..), TxId (..))
+import           Control.Exception                        (throw)
+import           Control.Monad                            (void)
+import           Control.Monad.Reader                     (asks)
+import           Data.Default                             (def)
+import           Data.Map                                 (fromList)
+import           Data.Maybe                               (fromJust)
+import           Ledger                                   (TxOutRef (..), TxId (..))
+import           PlutusTx.Prelude                         (toBuiltin)
+import           Text.Hex                                 (decodeHex)
 
-import           Cardano.Server.Class              (HasServer (..), Env (..))
-import           Cardano.Server.Endpoints.Tx.Class (HasTxEndpoints(..))
-import           Cardano.Server.Error              (IsCardanoServerError (..))
-import           Cardano.Server.Input              (InputContext(..))
-import           Cardano.Server.Internal           (runAppM)
-import           Cardano.Server.Main               (runServer)
-import           Cardano.Server.Tx                 (mkTx, mkBalanceTx)
-import           CSL                               (TransactionUnspentOutputs)
-import           CSL.Class                         (fromCSL)
-import           ENCOINS.Core.OffChain             (beaconMintTx, beaconSendTx, encoinsTx)
-import           ENCOINS.Core.V1.OnChain           (hashRedeemer)
-import           ENCOINS.Core.V1.OffChain          (EncoinsRedeemerWithData, verifierPKH, postEncoinsPolicyTx, postStakingValidatorTx)
-import           ENCOINS.Core.OnChain              (beaconCurrencySymbol, encoinsSymbol, stakingValidatorAddress)
-import           EncoinsServer.Opts                (ServerMode(..), runWithOpts)
-import           PlutusAppsExtra.IO.Wallet                         (getWalletUtxos)
-import           PlutusAppsExtra.Scripts.CommonValidators          (alwaysFalseValidatorAddress)
-import           PlutusAppsExtra.Utils.Crypto                      (sign)
-import PlutusAppsExtra.IO.ChainIndex (getUtxosAt)
-import Text.Hex (decodeHex)
-import PlutusTx.Prelude (toBuiltin)
-import Cardano.Server.Utils.Logger (logSmth)
+import           Cardano.Server.Class                     (HasServer (..), Env (..))
+import           Cardano.Server.Endpoints.Tx.Class        (HasTxEndpoints(..))
+import           Cardano.Server.Error                     (IsCardanoServerError (..))
+import           Cardano.Server.Input                     (InputContext(..))
+import           Cardano.Server.Internal                  (runAppM)
+import           Cardano.Server.Main                      (runServer)
+import           Cardano.Server.Tx                        (mkTx)
+import           CSL                                      (TransactionUnspentOutputs)
+import           CSL.Class                                (fromCSL)
+import           ENCOINS.Core.OffChain                    (beaconMintTx, beaconSendTx, encoinsTx)
+import           ENCOINS.Core.V1.OnChain                  (hashRedeemer)
+import           ENCOINS.Core.V1.OffChain                 (EncoinsRedeemerWithData, verifierPKH, postEncoinsPolicyTx, postStakingValidatorTx)
+import           ENCOINS.Core.OnChain                     (beaconCurrencySymbol, encoinsSymbol, stakingValidatorAddress)
+import           EncoinsServer.Opts                       (ServerMode(..), runWithOpts)
+import           PlutusAppsExtra.IO.Wallet                (getWalletUtxos)
+import           PlutusAppsExtra.Scripts.CommonValidators (alwaysFalseValidatorAddress)
+import           PlutusAppsExtra.Utils.Crypto             (sign)
 
 runEncoinsServer :: IO ()
 runEncoinsServer = do
@@ -73,18 +70,6 @@ instance HasServer EncoinsServer where
         mkTx [] def [postStakingValidatorTx stakingParams 7]
 
     serverIdle = pure ()
-        -- void $ do        
-        -- txOutRef <- asks envAuxiliary
-        -- let encoinsParams = (beaconCurrencySymbol txOutRef, verifierPKH)
-        --     stakingParams = encoinsSymbol encoinsParams
-        -- addrs <- serverTrackedAddresses
-        -- utxos <- getWalletUtxos
-        -- cAddr <- getWalletAddr
-        -- let ctx = InputContextClient mempty utxos (TxOutRef (TxId "") 0) cAddr
-        -- catch (void $ mkTx addrs ctx [stakingCombineTx stakingParams zero 2]) $ \case
-        --     NotEnoughFunds v -> logMsg $ "Missing " <> pack (show v) <>" ADA."
-        --         <> "\nThe relay needs at least 10 ADA to function properly."
-        --     _ -> return ()
 
     serverTrackedAddresses = do
         ref <- asks envAuxiliary
@@ -96,33 +81,18 @@ instance HasTxEndpoints EncoinsServer where
 
     data TxEndpointsErrorOf EncoinsServer = CorruptedExternalUTXOs | IncorrectVerifierSignature
         deriving (Show)
- 
+
     txEndpointsProcessRequest (redWithData@(addr, _), utxosCSL) = do
         let utxos   = maybe (throw CorruptedExternalUTXOs) fromList $ fromCSL utxosCSL
-            -- context = InputContextServer utxos
             context = InputContextClient utxos utxos
                 (TxOutRef (TxId "") 1)
                 addr
-        logSmth utxos
-        
-        addrs <- serverTrackedAddresses
-        builders <- txEndpointsTxBuilders redWithData
-        tx <- mkBalanceTx addrs context builders
-        logSmth tx
         return (redWithData, context)
 
     txEndpointsTxBuilders (addr, red@(par, input, proof, _)) = do
         bcs <- asks $ beaconCurrencySymbol . envAuxiliary
-        addrs <- serverTrackedAddresses
-        utxos <- liftIO $ mapM getUtxosAt addrs
         let prvKey = toBuiltin $ fromJust $ decodeHex "1DA4194798C1D3AA8B7E5E39EDA1F130D9123ACCC8CA31A82E033A6D007DA7EC"
             red' = (par, input, proof, sign prvKey $ hashRedeemer red)
-        liftIO $ print bcs
-        liftIO $ print prvKey
-        liftIO $ print $ hashRedeemer red
-        liftIO $ print $ sign prvKey $ hashRedeemer red
-        liftIO $ print par
-        liftIO $ print utxos
         pure [encoinsTx (bcs, verifierPKH) (addr, red')]
 
 instance IsCardanoServerError (TxEndpointsErrorOf EncoinsServer) where
