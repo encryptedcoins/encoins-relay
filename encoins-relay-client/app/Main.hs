e9663d7d6dd97b39e306908623b29bd661d035da
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-08-03 17:50:20 +0300

    Fix client executable

--- encoins-relay-client/app/Main.hs
+++ encoins-relay-client/app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import  Encoins.Relay.Client.Main (runEncoinsClient)
+import           Cardano.Server.Config         (decodeOrErrorFromFile, cAuxiliaryEnvFile)
+import           Cardano.Server.Client.Client  (runClientWithOpts)
+import           Encoins.Relay.Client.Client   (mkClientHandle)
+import           Encoins.Relay.Client.Opts     (Options (..), extractCommonOptions, runWithOpts)
+import           Encoins.Relay.Server.Config   (cVerifierConfig)
+import           Encoins.Relay.Server.Server   (mkServerHandle)
+import           Encoins.Relay.Verifier.Server (cBulletproofSetupFilePath)
+import           System.Directory              (createDirectoryIfMissing)
 
 main :: IO ()
-main = runEncoinsClient "config.json"
+main =  do
+    createDirectoryIfMissing True "secrets"
+    c                <- decodeOrErrorFromFile "config.json"
+    bulletproofSetup <- decodeOrErrorFromFile "../verifier/bulletproof_setup.json"
+    opts             <- runWithOpts
+    sh               <- mkServerHandle c
+    runClientWithOpts c sh (mkClientHandle bulletproofSetup (optsEncoinsMode opts)) $ extractCommonOptions opts
