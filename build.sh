3b0aa6d45d92fbc86e2fbb33f121055e1102d6b2
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-10-31 14:00:23 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/encoins-relay-server-0.1.0.0/buil
 
 cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/encoins-relay-verifier-0.1.0.0/build/encoins-relay-verifier/encoins-relay-verifier ~/.local/bin/encoins-verifier
 
-cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/encoins-relay-client-0.1.0.0/build/encoins-relay-client/encoins-relay-client ~/.local/bin/encoins-client
+cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/encoins-relay-client-0.1.0.0/x/encoins-relay-client/build/encoins-relay-client/encoins-relay-client ~/.local/bin/encoins-client
 
 cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/encoins-relay-apps-0.1.0.0/x/encoins-relay-poll/build/encoins-relay-poll/encoins-relay-poll ~/.local/bin/encoins-poll
 
