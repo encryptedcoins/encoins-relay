65767137ae68f00cba1ed2cb32ec019152e8a857
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-11-24 23:15:00 +0300

    Add debts to distributeRewards

--- encoins-relay-server/src/Encoins/Relay/Server/Delegation.hs
+++ encoins-relay-server/src/Encoins/Relay/Server/Delegation.hs
@@ -1,24 +1,30 @@
-{-# LANGUAGE ImplicitParams     #-}
-{-# LANGUAGE RecordWildCards    #-}
-{-# LANGUAGE TupleSections      #-}
+{-# LANGUAGE ImplicitParams    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Encoins.Relay.Server.Delegation where
 
+import           Cardano.Api                          (writeFileJSON)
+import           Cardano.Server.Config                (decodeOrErrorFromFile)
 import           Cardano.Server.Internal              (ServerM, getAuxillaryEnv)
 import           Cardano.Server.Tx                    (mkTx)
+import           Control.Applicative                  ((<|>))
 import           Control.Monad                        (void, when)
 import           Control.Monad.IO.Class               (MonadIO (..))
 import           Control.Monad.State                  (modify)
 import           Data.Bifunctor                       (Bifunctor (..))
 import           Data.Default                         (Default (..))
+import           Data.Function                        (on)
 import           Data.Functor                         ((<&>))
-import           Data.List.Extra                      (chunksOf)
+import           Data.List.Extra                      (chunksOf, partition, sort)
+import qualified Data.List.NonEmpty                   as NonEmpty
 import qualified Data.Map                             as Map
 import           Data.Maybe                           (mapMaybe)
 import           Encoins.Relay.Apps.Delegation.Client (mkDelegationClientEnv, serverDelegatesClient)
 import           Encoins.Relay.Server.Internal        (EncoinsRelayEnv (..))
 import           Encoins.Relay.Server.Server          (EncoinsApi)
-import           Ledger                               (Address (..), PaymentPubKeyHash (..))
+import           Ledger                               (Address (..), PaymentPubKeyHash (..), minAdaTxOutEstimated)
 import           Ledger.Tx.Constraints                (mustPayToPubKeyAddress)
 import           Plutus.Script.Utils.Ada              (Ada)
 import qualified Plutus.Script.Utils.Ada              as Ada
@@ -26,12 +32,14 @@ import           Plutus.V2.Ledger.Api                 (Credential (..))
 import           PlutusAppsExtra.Types.Tx             (TxConstructor (..))
 import           PlutusAppsExtra.Utils.Address        (bech32ToAddress)
 
-
 distributeRewards :: Ada -> ServerM EncoinsApi ()
 distributeRewards totalReward = void $ do
         distribution <- getRewardsDistribution totalReward
-        when (null distribution) $ error "Server doesn't has any delegates."
-        mapM_ sendFunds $ mkConstrs distribution
+        debts <- liftIO $ decodeOrErrorFromFile debtFile <|> pure []
+        let (distribution', debts') = partition ((> minAdaTxOutEstimated) . snd) $ concatDistrubutions distribution debts
+        when (null distribution' && null debts') $ error "Server doesn't has any delegates."
+        mapM_ sendFunds $ mkConstrs distribution'
+        liftIO $ writeFileJSON debtFile debts'
     where
         mkConstrs recepients = fmap mconcat $ chunksOf 5 $ flip mapMaybe recepients $ \(addr, reward) -> case addr of
             Address (PubKeyCredential pkh) (Just scred) -> Just $ mustPayToPubKeyAddress (PaymentPubKeyHash pkh) scred (Ada.toValue reward)
@@ -39,7 +47,9 @@ distributeRewards totalReward = void $ do
 
         sendFunds constrs = mkTx [] def (pure $ modify $ \constr -> constr{txConstructorResult = Just (mempty, constrs)})
 
-getRewardsDistribution ::Ada -> ServerM EncoinsApi [(Address, Ada)]
+        debtFile = "debts.json"
+
+getRewardsDistribution :: Ada -> ServerM EncoinsApi [(Address, Ada)]
 getRewardsDistribution totalReward = calculateReward <$> getRecepients
     where
         getRecepients = mapMaybe (\(addrTxt, b) -> (, b) <$> bech32ToAddress addrTxt) <$> do
@@ -51,3 +61,10 @@ getRewardsDistribution totalReward = calculateReward <$> getRecepients
         calculateReward recepients =
             let totalBalance = sum $ snd <$> recepients
             in recepients <&> second (fromInteger . (`div` totalBalance) . (* fromIntegral totalReward))
+
+concatDistrubutions :: [(Address, Ada)] -> [(Address, Ada)] -> [(Address, Ada)]
+concatDistrubutions distribution debts
+    = map (\xs -> (fst $ NonEmpty.head xs, sum $ snd <$> xs))
+    $ NonEmpty.groupBy ((==) `on` fst)
+    $ sort
+    $ distribution <> debts
--- encoins-relay-test/test/Encoins/Relay/DelegationSpec.hs
+++ encoins-relay-test/test/Encoins/Relay/DelegationSpec.hs
@@ -39,7 +39,8 @@ propCurrent :: HasServantClientEnv => Expectation
 propCurrent = currentServersClient >>= (`shouldBe` Right ["test.delegation"])
 
 propDelegatesOk :: HasServantClientEnv => Expectation
-propDelegatesOk = serverDelegatesClient "test.delegation" >>= (`shouldBe` Right (Map.fromList [("addr_test1qrzde3sqw0na9n4p8dmhc55gkphk2x48hzclpjx6cy0wuzgnt9a4spnfrrlpp7puw2lcx2zudf49ewyza4q9ha08qhdqghxhek", 3)]))
+propDelegatesOk = serverDelegatesClient "test.delegation" >>=
+    (`shouldBe` Right (Map.fromList [("addr_test1qrzde3sqw0na9n4p8dmhc55gkphk2x48hzclpjx6cy0wuzgnt9a4spnfrrlpp7puw2lcx2zudf49ewyza4q9ha08qhdqghxhek", 3)]))
 
 propDelegates404 :: HasServantClientEnv => Expectation
 propDelegates404 = serverDelegatesClient "non-existent.delegation" >>= (`shouldBe` Left (DelegationServerError UnknownIp))
