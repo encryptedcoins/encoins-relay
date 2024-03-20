f3797b6a492fd05bb0aa966ea060e2fd1800b2da
Cauterization <71918928+Cauterization@users.noreply.github.com>
2024-03-20 19:56:20 +0300

    Update deps, fix reward tx
    Add metadata to reward tx, fix confirmation message

--- encoins-relay-server/src/Encoins/Relay/Server/Delegation.hs
+++ encoins-relay-server/src/Encoins/Relay/Server/Delegation.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ImplicitParams    #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE TypeFamilies      #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Redundant <$>" #-}
 
@@ -10,7 +12,7 @@ module Encoins.Relay.Server.Delegation where
 
 import           Cardano.Api                          (writeFileJSON)
 import           Cardano.Server.Config                (decodeOrErrorFromFile)
-import           Cardano.Server.Internal              (Env (..), ServerM, getAuxillaryEnv, mkServantClientEnv)
+import           Cardano.Server.Internal              (Env (..), ServerM, getAuxillaryEnv, mkServantClientEnv, getNetworkId)
 import           Cardano.Server.Tx                    (mkBalanceTx, mkTx)
 import           Cardano.Server.Utils.Logger          (logMsg, logPretty)
 import           Control.Applicative                  ((<|>))
@@ -26,7 +28,7 @@ import           Data.Functor                         ((<&>))
 import           Data.List.Extra                      (chunksOf, partition, sort)
 import qualified Data.List.NonEmpty                   as NonEmpty
 import qualified Data.Map                             as Map
-import           Data.Maybe                           (mapMaybe)
+import           Data.Maybe                           (mapMaybe, fromMaybe)
 import qualified Data.Text                            as T
 import           Encoins.Relay.Apps.Delegation.Client (serverDelegatesClient)
 import           Encoins.Relay.Server.Internal        (EncoinsRelayEnv (..))
@@ -38,7 +40,8 @@ import qualified Plutus.Script.Utils.Ada              as Ada
 import           Plutus.V2.Ledger.Api                 (Credential (..))
 import           PlutusAppsExtra.IO.Wallet            (HasWalletProvider (..))
 import           PlutusAppsExtra.Types.Tx             (TxConstructor (..))
-import           PlutusAppsExtra.Utils.Address        (bech32ToAddress)
+import           PlutusAppsExtra.Utils.Address        (bech32ToAddress, addressToBech32)
+import           PlutusAppsExtra.Utils.Tx             (mkCip20Metadata)
 
 distributeRewards :: Ada -> ServerM EncoinsApi ()
 distributeRewards totalReward = void $ do
@@ -47,25 +50,50 @@ distributeRewards totalReward = void $ do
         let (distribution', debts') = partition ((> minAdaTxOutEstimated) . snd) $ concatDistrubutions distribution debts
         when (null distribution' && null debts') $ error "Server doesn't has any delegates."
         addrs <- getWalletAddresses
-        mapM_ (sendFunds addrs) $ mkConstrs distribution'
+        let constrsWithRewards = mkConstrsWithReward distribution'
+        mkConfirmationMsg distribution' debts' (snd <$> constrsWithRewards)
+        doConfirmation
+        mapM_ (uncurry $ sendFunds addrs) constrsWithRewards
         liftIO $ writeFileJSON debtFile debts'
     where
-        mkConstrs recepients = fmap mconcat $ chunksOf maxParticipants $ flip mapMaybe recepients $ \(addr, reward) -> case addr of
-            Address (PubKeyCredential pkh) (Just scred) -> Just $ mustPayToPubKeyAddress (PaymentPubKeyHash pkh) scred (Ada.toValue reward)
+        mkConstrsWithReward recepients = fmap mconcat $ chunksOf maxParticipants $ flip mapMaybe recepients $ \(addr, reward) -> case addr of
+            Address (PubKeyCredential pkh) (Just scred) -> Just $ (, reward) $ mustPayToPubKeyAddress (PaymentPubKeyHash pkh) scred (Ada.toValue reward)
             _ -> Nothing
 
-        sendFunds addrs constrs = do
+        sendFunds addrs constrs reward = do
+            relayUrl <- envDelegationIp <$> getAuxillaryEnv
+            metaData <- either (error . show) (pure . Just) $ mkCip20Metadata [relayUrl, T.pack $ show $ toInteger reward]
             let txBuilder = [modify $ \constr -> constr{txConstructorResult = Just (mempty, constrs)}]
-            tx <- mkBalanceTx addrs def txBuilder
+            tx <- mkBalanceTx addrs def txBuilder metaData
             logPretty tx
-            logMsg $ "Please enter \"Y\" to complete the above transaction or \"N\" to cancel.\n\
-                   \Keep in mind that each transaction contains no more than "
-                   <> T.pack (show maxParticipants)
-                   <> " participants."
-            doConfirmation
-            mkTx addrs def txBuilder
+            mkTx addrs def txBuilder metaData
 
         debtFile = "debts.json"
+
+        mkConfirmationMsg distribution debts rewards = do
+            rewardUrl <- envDelegationIp <$> getAuxillaryEnv
+            networkId <- getNetworkId
+            let txsNum = length rewards
+                onMultipleTxs single multiple = if txsNum == 1 then single else multiple
+                txs = T.pack (show txsNum) <> " transaction" <> onMultipleTxs "" "s"
+                reward = renderAda (sum rewards)
+                rewardSplitted =  " (" <> T.intercalate ", " (map renderAda rewards) <> ")"
+                renderedDistribution = fmap (\(a,r) -> fromMaybe (T.pack $ show a) (addressToBech32 networkId a) <> " : " <> renderAda r)
+            logMsg $ mconcat
+                [ "Please enter \"Y\" to complete ", txs
+                , " and distribute rewards - ", reward, onMultipleTxs "" rewardSplitted
+                , " between ", rewardUrl, " delegates "
+                , "or \"N\" to cancel.\n"
+                , "Keep in mind that each transaction contains no more than "
+                , T.pack (show maxParticipants), " participants.\n\n"
+                , rewardUrl, " delegates:\n"
+                , T.intercalate "\n" $ renderedDistribution distribution
+                , "\n\nRewards of these addresses are too small to be included. They will be added to debt file and rewarded in next distributions:\n"
+                , T.intercalate "\n" $ renderedDistribution debts
+                ]
+
+        renderAda = T.pack . (\(f, w) -> case w of {"" -> "0"; _ -> w}  <> "." <> f <> "₳")
+            . bimap reverse reverse . splitAt 6 . reverse . show . toInteger
 
         doConfirmation = do
             map toUpper <$> liftIO getLine >>= \case
