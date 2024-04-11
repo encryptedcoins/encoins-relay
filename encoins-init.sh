4e3bec98ccb93b85349d908f17ce4dedb34eb381
Cauterization <71918928+Cauterization@users.noreply.github.com>
2024-04-11 13:03:51 +0300

    Update encoins-init.sh

--- encoins-init.sh
+++ encoins-init.sh
@@ -12,10 +12,26 @@ chmod +x encoins
 cp encoins ~/.local/bin/encoins
 
 # Download and extract configs
-echo "https://github.com/encryptedcoins/encoins-relay/archive/${GITHUB_TAG}.tar.gz"
-wget "https://github.com/encryptedcoins/encoins-relay/archive/${GITHUB_TAG}.tar.gz"
-tar -xf "${GITHUB_TAG}.tar.gz"
+git clone https://github.com/encryptedcoins/encoins-relay
+cd encoins-relay || exit
+
+if [[ "${GITHUB_TAG}" == "v1.2.5.1" ]]
+then
+    git checkout 46608dd67ce79597c2ae4c110cb8411af2d8f7cf
+else
+    git checkout "${GITHUB_TAG}"
+fi
+
+ls
+
+mv  -v  encoins-relay-server/app_lightweight/mainnet/* ../
+cd .. || exit
+
+ls
+
+# Remove trash
+rm encoins
+rm -r -f encoins-relay
 
 # Run app
-cd "encoins-relay-${GITHUB_TAG}/encoins-relay-server/app_lightweight" || exit
-./run.sh
+encoins
