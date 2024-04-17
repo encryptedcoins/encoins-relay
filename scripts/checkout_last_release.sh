0a36820c623e720c5ccfea04f2ce8c0319466a58
Cauterization <71918928+Cauterization@users.noreply.github.com>
2024-04-17 08:31:19 +0300

    Update checkout_last_release.sh

--- scripts/checkout_last_release.sh
+++ scripts/checkout_last_release.sh
@@ -9,7 +9,7 @@ latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 # Checkout latest tag
 if [[ "${latestTag}" == "v1.2.5.1" ]]
 then
-    git checkout a13656c9107ffcbacd161f48762c26a30fc9efc3
+    git checkout 77aa006c07b18f4794cf9ec34fef1f8edbfbe51a
 else
     git checkout "${latestTag}"
 fi
